7b40cbbfd878a59b61b2e0839db174a685f2691c
timbutokawa <tim@timbutorac.com>
2015-04-24 08:16:19 -0700

    add page folders and begin templates

--- src/app/index.js
+++ src/app/index.js
@@ -4,13 +4,38 @@
     angular.module('cmlee', ['ui.router', 'ui.bootstrap'])
         .config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider
-            .state('home', {
-                url: '/',
+            .state('main', {
+                url: '/home',
                 templateUrl: 'app/main/main.html',
                 controller: 'Main as main'
-            });
+            })
+            .state('statement', {
+                url: '/statement',
+                templateUrl: 'app/statement/statement.html'
+            })
+            .state('contact', {
+                url: '/contact',
+                templateUrl: 'app/contact/contact.html'
+            })
+            .state('cv', {
+                url: '/cv',
+                templateUrl: 'app/cv/cv.html'
+            })
+            .state('paper', {
+                url: '/paper',
+                templateUrl: 'app/paper/paper.html'
+            })
+            .state('wall', {
+                url: '/wall',
+                templateUrl: 'app/wall/wall.html'
+            })
+            .state('old', {
+                url: '/old',
+                templateUrl: 'app/old/old.html'
+            })
+            ;
 
-            $urlRouterProvider.otherwise('/');
+            $urlRouterProvider.otherwise('/home');
         })
     ;
 })();
